b052f0d07d26fe07764c6d2455ea73bd6b50194a
c0d3h01 <c0d3h01@gmail.com>
2025-03-26 14:33:08 +0530

    upd: enhance install script with improved logging and cleanup steps

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,21 +5,25 @@ SCRIPT_DIR=~/dotfiles
 
 # Check if dotfiles repo exists, clone if not
 if [ ! -d "$SCRIPT_DIR/.git" ]; then
-  echo "Cloning dotfiles..."
-  git clone https://github.com/c0d3h01/dotfiles $SCRIPT_DIR
+  echo "-*-> Cloning dotfiles..."
+  git clone https://github.com/c0d3h01/dotfiles $SCRIPT_DIR 
 fi
 
 # Change to dotfiles directory
 cd $SCRIPT_DIR || exit 1
 
 # Ensure hardware configuration exists
-if [ ! -f "$SCRIPT_DIR/nix/hardware.nix" ]; then
-  echo "Generating hardware configuration..."
-  sudo nixos-generate-config --show-hardware-config >$SCRIPT_DIR/nix/hardware.nix
-fi
+echo "-*-> Generating hardware configuration..."
+sudo mount /dev/nvme0n1p1 /boot && echo "Mounted /boot"
+sudo nixos-generate-config --show-hardware-config >$SCRIPT_DIR/nix/hardware.nix && echo "Generated hardware.nix"
+
+# Cleanup Xdg user directories
+echo "-*-> Cleaning up XDG directories..."
+rm ~/.config/user-dirs.dirs* && echo "Removed user-dirs.dirs"
+xdg-user-dirs-update && echo "Updated XDG directories"
 
 # Rebuild system using flake
-echo "Applying system configuration..."
+echo "-*-> Applying system configuration..."
 sudo nixos-rebuild switch --flake .#NixOS --upgrade --show-trace --option experimental-features "nix-command flakes"
 
-echo "✅ Setup complete! Your system is now configured."
+echo "*** Setup complete! Your system is now configured. ***"
