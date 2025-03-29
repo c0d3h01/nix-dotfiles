989c0d43285f78312b78136c9ece566404007648
c0d3h01 <c0d3h01@gmail.com>
2025-03-30 00:01:35 +0530

    upd: improve formatting of hardware configuration for better readability

--- nix/hardware-configuration.nix
+++ nix/hardware-configuration.nix
@@ -5,7 +5,8 @@
 
 {
   imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "sd_mod" ];
@@ -14,19 +15,19 @@
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/fdc9a02e-7063-47ce-9efe-1ca42859b4f9";
+    {
+      device = "/dev/disk/by-uuid/fdc9a02e-7063-47ce-9efe-1ca42859b4f9";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/23E8-FDF3";
+    {
+      device = "/dev/disk/by-uuid/23E8-FDF3";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/fe010f86-99b7-4b56-be4d-6a84682980ec"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
