19388e5b10210460811ffde5d9ab457c7e396235
c0d3h01 <c0d3h01@gmail.com>
2025-04-06 09:50:47 +0530

    feat: python - add packages (virtualenv, jupyter, scipy, pandas, scikit-learn, matplotlib, torch)

--- modules/development/python.nix
+++ modules/development/python.nix
@@ -5,9 +5,15 @@
   environment.systemPackages = with pkgs; [
     (pkgs.python312.withPackages (ps: with ps; [
       pip
-      jupyterlab
+      virtualenv
+      jupyter
       sympy
       numpy
+      scipy
+      pandas
+      scikit-learn
+      matplotlib
+      torch
     ]))
     pyright
     ruff
