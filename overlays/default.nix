df1037a9288490382879bf8c48b21bf0139f58ee
c0d3h01 <harshalsawant.dev@gmail.com>
2025-04-28 19:00:35 +0530

    stye: diff

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,11 +1,10 @@
 { inputs }:
-{
 
-  default = final: prev: {
-    # Stable Nixpkgs config, i use unstable as default
+[
+  (final: prev: {
     stable = import inputs.nixpkgs-stable {
       system = prev.stdenv.system;
       config.allowUnfree = true;
     };
-  };
-}
+  })
+]
