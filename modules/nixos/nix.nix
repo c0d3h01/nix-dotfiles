9b9ff28708b13776c81c1640d2c236ba8bdd12f3
c0d3h01 <harshalsawant.dev@gmail.com>
2025-05-02 18:18:00 +0530

    fix: filesystem and removing unused Cachix key

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -9,10 +9,14 @@
   system.stateVersion = userConfig.stateVersion;
   networking.hostName = userConfig.hostname;
 
+  fileSystems."/home" = {
+    device = "/dev/sda1";
+    fsType = "ext4";
+  };
+
   nixpkgs = {
     overlays = outputs.overlays;
     config = {
-      allowUnfreePredicate = _: true;
       allowUnfree = true;
       android_sdk.accept_license = true;
     };
@@ -40,13 +44,11 @@
       substituters = [
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
-        "https://hyprland.cachix.org"
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
