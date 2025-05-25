673f3165ce9d391860c38d1511ae8a47d5daef89
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-05-25 10:31:30 -0400

    chore(flake): upd stateVersion changed [ 25.05 ]

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     home-manager.url = "github:nix-community/home-manager";
@@ -47,7 +47,7 @@
         fullName = "Harshal Sawant";
         email = "harshalsawant.dev@gmail.com";
         hostname = "NixOS";
-        stateVersion = "24.11";
+        stateVersion = "25.05";
       };
     in
     {
