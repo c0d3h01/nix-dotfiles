a514aeda58f6ca00c124dcfedcec053f7ce6ac4d
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-14 14:49:18 +0530

    refactor(tweaks): streamline power management settings and service configurations

--- nixosModules/tweaks.nix
+++ nixosModules/tweaks.nix
@@ -1,9 +1,17 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 {
-  powerManagement.powertop.enable = lib.mkDefault true;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "schedutil";
-  services.thermald.enable = lib.mkDefault true;
-  services.earlyoom.enable = lib.mkDefault true;
+  powerManagement.powertop.enable = true;
+  systemd.services.powertop.postStart = ''
+    HIDDEVICES=$(ls /sys/bus/usb/drivers/usbhid | grep -oE '^[0-9]+-[0-9\.]+' | sort -u)
+    for i in $HIDDEVICES; do
+      echo -n "Enabling " | cat - /sys/bus/usb/devices/$i/product
+      echo 'on' > /sys/bus/usb/devices/$i/power/control
+    done
+  '';
+
+  systemd.services.tlp = lib.mkIf (config.services.tlp.enable) { after = [ "powertop.service" ]; };
+
+  # powerManagement.cpuFreqGovernor = lib.mkDefault "schedutil";
   systemd.oomd.enable = true;
 }
