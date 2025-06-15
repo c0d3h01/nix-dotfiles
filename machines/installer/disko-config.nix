641acd69ce74cc0f50cf105881aed148eaa5a3d9
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-06-15 20:24:24 +0530

    refactor(disko-config.nix): subvolumes

--- machines/installer/disko-config.nix
+++ machines/installer/disko-config.nix
@@ -8,94 +8,48 @@
           type = "gpt";
           partitions = {
             ESP = {
-              name = "Nix_EFI";
+              name = "nixos_EFI";
               size = "512M";
               type = "EF00";
               content = {
                 type = "filesystem";
                 format = "vfat";
-                mountpoint = "/boot";
+                mountpoint = "/boot/efi";
                 mountOptions = [ "umask=0077" ];
               };
             };
             root = {
-              name = "Nix_root";
+              name = "nixos_root";
               size = "100%";
               content = {
                 type = "btrfs";
-                extraArgs = [
-                  "-f"
-                ];
+                extraArgs = [ "-f" ];
                 subvolumes = {
                   "/@" = {
                     mountpoint = "/";
                     mountOptions = [
-                      "compress=zstd:3"
-                      "noatime"
+                      "compress=zstd"
                       "ssd"
                       "space_cache=v2"
                       "commit=120"
-                      "discard=async"
                     ];
                   };
                   "/@home" = {
                     mountpoint = "/home";
                     mountOptions = [
-                      "compress=zstd:5"
-                      "noatime"
+                      "compress=zstd"
                       "ssd"
                       "space_cache=v2"
                       "commit=120"
-                      "discard=async"
-                      "autodefrag"
                     ];
                   };
                   "/@nix" = {
                     mountpoint = "/nix";
                     mountOptions = [
-                      "compress=zstd:1"
-                      "noatime"
-                      "ssd"
-                      "space_cache=v2"
-                      "commit=300"
-                      "discard=async"
-                    ];
-                  };
-                  "/@var" = {
-                    mountpoint = "/var";
-                    mountOptions = [
-                      "compress=zstd:3"
-                      "noatime"
+                      "compress=zstd"
                       "ssd"
                       "space_cache=v2"
                       "commit=120"
-                      "discard=async"
-                      "autodefrag"
-                      "ssd_spread"
-                    ];
-                  };
-                  "/@log" = {
-                    mountpoint = "/var/log";
-                    mountOptions = [
-                      "compress=zstd:9"
-                      "noatime"
-                      "ssd"
-                      "space_cache=v2"
-                      "commit=60"
-                      "discard=async"
-                      "autodefrag"
-                      "ssd_spread"
-                    ];
-                  };
-                  "/@tmp" = {
-                    mountpoint = "/tmp";
-                    mountOptions = [
-                      "compress=no"
-                      "noatime"
-                      "ssd"
-                      "nosuid"
-                      "nodev"
-                      "discard=async"
                     ];
                   };
                 };
