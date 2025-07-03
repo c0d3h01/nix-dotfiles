6624b57b7c1daa558b8eff1047d56c0c57837f69
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-07-03 19:32:48 +0530

    gc: rm unused agenix reference

--- flake.nix
+++ flake.nix
@@ -3,17 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-
     systems.url = "github:nix-systems/default";
-
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
-    agenix.url = "github:ryantm/agenix";
-    agenix.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     sops-nix.url = "github:c0d3h01/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
