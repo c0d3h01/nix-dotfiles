9dfc4fc7bf8944f74bd6619286cef207a42c45f8
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-07-31 16:37:21 +0000

    revert all the disko changes

--- flake.nix
+++ flake.nix
@@ -49,15 +49,11 @@
       home-manager,
       ...
     }:
-    let
-      diskoConfig = import ./nixosConfigs/modules/users/c0d3h01/disko-btrfs.nix;
-    in
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./flake
         ./homeManager/flake-module.nix
         ./nixosConfigs/flake-module.nix
       ];
-      diskoConfigurations = diskoConfig;
     };
 }
