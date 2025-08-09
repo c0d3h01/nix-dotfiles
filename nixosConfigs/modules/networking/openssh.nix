157e22ce11897fe1ac8a880e52c4aac01b4f6460
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-09 14:01:44 +0530

    clean up opensshconfig

--- nixosConfigs/modules/networking/openssh.nix
+++ nixosConfigs/modules/networking/openssh.nix
@@ -4,15 +4,12 @@
     enable = true;
     startWhenNeeded = true;
     allowSFTP = true;
-
     openFirewall = true;
     # the port(s) openssh daemon should listen on
     ports = [ 22 ];
-
     settings = {
       # Don't allow root login
       PermitRootLogin = "no";
-
       # only allow key based logins and not password
       PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
@@ -20,10 +17,8 @@
       PubkeyAuthentication = "yes";
       ChallengeResponseAuthentication = "no";
       UsePAM = false;
-
       UseDns = false;
       X11Forwarding = false;
-
       # Use key exchange algorithms recommended by `nixpkgs#ssh-audit`
       KexAlgorithms = [
         "curve25519-sha256"
@@ -35,19 +30,16 @@
         "mlkem768x25519-sha256"
         "sntrup761x25519-sha512"
       ];
-
       # Use Macs recommended by `nixpkgs#ssh-audit`
       Macs = [
         "hmac-sha2-512-etm@openssh.com"
         "hmac-sha2-256-etm@openssh.com"
         "umac-128-etm@openssh.com"
       ];
-
       # kick out inactive sessions
       ClientAliveCountMax = 5;
       ClientAliveInterval = 60;
     };
-
     hostKeys = [
       {
         bits = 4096;
@@ -55,46 +47,9 @@
         type = "rsa";
       }
       {
-        bits = 4096;
         path = "/etc/ssh/ssh_host_ed25519_key";
         type = "ed25519";
       }
     ];
-
-    # find these with `ssh-keyscan <hostname>`
-    # knownHosts = {
-    #   "github.com" = [
-    #     {
-    #       type = "rsa";
-    #       key = "AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=";
-    #     }
-    #     {
-    #       type = "ed25519";
-    #       key = "AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
-    #     }
-    #   ];
-
-    #   "gitlab.com" = [
-    #     {
-    #       type = "rsa";
-    #       key = "AAAAB3NzaC1yc2EAAAADAQABAAABAQCsj2bNKTBSpIYDEGk9KxsGh3mySTRgMtXL583qmBpzeQ+jqCMRgBqB98u3z++J1sKlXHWfM9dyhSevkMwSbhoR8XIq/U0tCNyokEi/ueaBMCvbcTHhO7FcwzY92WK4Yt0aGROY5qX2UKSeOvuP4D6TPqKF1onrSzH9bx9XUf2lEdWT/ia1NEKjunUqu1xOB/StKDHMoX4/OKyIzuS0q/T1zOATthvasJFoPrAjkohTyaDUz2LN5JoH839hViyEG82yB+MjcFV5MU3N1l1QL3cVUCh93xSaua1N85qivl+siMkPGbO5xR/En4iEY6K2XPASUEMaieWVNTRCtJ4S8H+9";
-    #     }
-    #     {
-    #       type = "ed25519";
-    #       key = "AAAAC3NzaC1lZDI1NTE5AAAAIAfuCHKVTjquxvt6CM6tdG4SLp1Btn/nOeHHE5UOzRdf";
-    #     }
-    #   ];
-
-    #   "codeberg.org" = [
-    #     {
-    #       type = "rsa";
-    #       key = "AAAAB3NzaC1yc2EAAAADAQABAAABAQC8hZi7K1/2E2uBX8gwPRJAHvRAob+3Sn+y2hxiEhN0buv1igjYFTgFO2qQD8vLfU/HT/P/rqvEeTvaDfY1y/vcvQ8+YuUYyTwE2UaVU5aJv89y6PEZBYycaJCPdGIfZlLMmjilh/Sk8IWSEK6dQr+g686lu5cSWrFW60ixWpHpEVB26eRWin3lKYWSQGMwwKv4LwmW3ouqqs4Z4vsqRFqXJ/eCi3yhpT+nOjljXvZKiYTpYajqUC48IHAxTWugrKe1vXWOPxVXXMQEPsaIRc2hpK+v1LmfB7GnEGvF1UAKnEZbUuiD9PBEeD5a1MZQIzcoPWCrTxipEpuXQ5Tni4mN";
-    #     }
-    #     {
-    #       type = "ed25519";
-    #       key = "AAAAC3NzaC1lZDI1NTE5AAAAIIVIC02vnjFyL+I4RHfvIGNtOgJMe769VTF1VR4EB3ZB";
-    #     }
-    #   ];
-    # };
   };
 }
