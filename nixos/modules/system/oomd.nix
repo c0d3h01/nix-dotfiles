1920f60e93673c3c2bd828d2340acc6e194eb04d
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-15 00:28:50 +0530

    refactor(oomd): remove unused tmpfiles settings for kernel log storage

--- nixos/modules/system/oomd.nix
+++ nixos/modules/system/oomd.nix
@@ -11,19 +11,5 @@
     };
 
     services.nix-daemon.serviceConfig.OOMScoreAdjust = lib.mkDefault 350;
-
-    tmpfiles.settings."10-oomd-root".w = {
-      # Enables storing of the kernel log (including stack trace) into pstore upon a panic or crash.
-      "/sys/module/kernel/parameters/crash_kexec_post_notifiers" = {
-        age = "-";
-        argument = "Y";
-      };
-
-      # Enables storing of the kernel log upon a normal shutdown (shutdown, reboot, halt).
-      "/sys/module/printk/parameters/always_kmsg_dump" = {
-        age = "-";
-        argument = "N";
-      };
-    };
   };
 }
