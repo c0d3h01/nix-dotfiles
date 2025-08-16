b38d7d7044ef3a5911ad850c07b6b3c5bde86dc5
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-08-16 14:36:23 +0530

    chore(flatpak): disable flatpak service in workstation configuration

--- nixos/modules/system/flatpak.nix
+++ nixos/modules/system/flatpak.nix
@@ -6,7 +6,7 @@
 {
   # flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
   config = lib.mkIf userConfig.machineConfig.workstation.enable {
-    services.flatpak.enable = true;
+    services.flatpak.enable = false;
     environment.sessionVariables.XDG_DATA_DIRS = [ "/var/lib/flatpak/exports/share" ];
   };
 }
