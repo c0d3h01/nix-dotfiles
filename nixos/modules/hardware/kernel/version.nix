b665743ba5c40dee5cdd1536de04cbc667aaf7a2
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-10-23 18:29:01 +0530

    feat: add cachyos kernel

--- nixos/modules/hardware/kernel/version.nix
+++ nixos/modules/hardware/kernel/version.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }:
 {
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_stable;
+  # boot.kernelPackages = pkgs.linuxPackages_xanmod_stable;
+  boot.kernelPackages = pkgs.linuxPackages_cachyos;
 }
