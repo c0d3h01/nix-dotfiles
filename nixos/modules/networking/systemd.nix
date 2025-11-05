cb0c314f9503a9a56b3625ea5cdbe9cf3b3f2403
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-11-05 08:18:35 +0000

    feat: dns nameserver configuration

--- nixos/modules/networking/systemd.nix
+++ nixos/modules/networking/systemd.nix
@@ -1,13 +1,21 @@
 {
   # systemd DNS resolver daemon
-  services.resolved.enable = true;
+  networking.nameservers = [
+    "1.1.1.1"
+    "1.0.0.1"
+  ];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1"
+      "1.0.0.1"
+    ];
+    dnsovertls = "true";
+  };
 
   # Faster boot: don't block on network-online
   systemd.network.wait-online.enable = false;
   systemd.services.NetworkManager-wait-online.enable = false;
-
-  # disable networkd and resolved from being restarted on configuration changes
-  # also prevents failures from services that are restarted instead of stopped
-  systemd.services.systemd-networkd.stopIfChanged = false;
-  systemd.services.systemd-resolved.stopIfChanged = false;
 }
