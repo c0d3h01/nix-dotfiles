1efc825bc7fe96e252b34f72ec20dee709077ade
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-11-24 14:40:39 +0530

    Remove obsolete Zsh configuration files and plugins
    - Deleted .zsh-autosuggestions, .zsh-completions, .zsh-syntax-highlighting, .zsh-termsupport, and .zshrc files.
    - Removed associated plugin directories for fzf, fzf-tab, zsh-completions, and zsh-syntax-highlighting.
    - Cleaned up the environment by eliminating unused configurations and plugins.

--- home/.config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
-require("config.options")
-require("config.keybinds")
-require("config.lazy")
-require("config.autocmd")
-
--- Disable diagnostic signs (E, W, H, I) displayed to the left of row numbers
-vim.diagnostic.config({
-	signs = false,
-})
