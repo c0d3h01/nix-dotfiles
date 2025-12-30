d9be161bcf99d2207bc3590a879cdcfb306c7b76
Harshal Sawant <harshalsawant.dev@gmail.com>
2025-12-30 09:45:54 +0530

    fix: add --recurse-submodules option to git clone command

--- home-manager/modules/dotfiles.nix
+++ home-manager/modules/dotfiles.nix
@@ -12,7 +12,7 @@
     if [ ! -d "$DOTFILES_DIR" ]; then
       echo "Dotfiles directory not found. Cloning and stowing..."
       # Clone the repo
-      $DRY_RUN_CMD ${pkgs.git}/bin/git clone "$REPO_URL" "$DOTFILES_DIR"
+      $DRY_RUN_CMD ${pkgs.git}/bin/git clone --recurse-submodules "$REPO_URL" "$DOTFILES_DIR"
       $DRY_RUN_CMD ${pkgs.stow}/bin/stow -d "$DOTFILES_DIR" -t "${config.home.homeDirectory}" .
     else
       echo "Dotfiles directory exists. Skipping clone and stow."
