e4a9bfdca13f326247bd3cb85ea99ba1fd52cae3
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-01-24 20:10:30 +0530

    chore: rm overhead flatpak service

--- nixos/modules/system/flatpak.nix
+++ nixos/modules/system/flatpak.nix
@@ -7,14 +7,5 @@
   # flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
   config = lib.mkIf userConfig.workstation {
     services.flatpak.enable = true;
-    systemd.services.flatpak-repo = {
-      wantedBy = ["multi-user.target"];
-      path = [pkgs.flatpak];
-      after = ["network-online.target"];
-      wants = ["network-online.target"];
-      script = ''
-        flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-      '';
-    };
   };
 }
