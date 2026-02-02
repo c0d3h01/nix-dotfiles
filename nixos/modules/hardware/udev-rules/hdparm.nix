4188876d830cb285f7f14a00ac0ce3865c2999c0
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-02 05:48:27 +0530

    nix fmt

--- nixos/modules/hardware/udev-rules/hdparm.nix
+++ nixos/modules/hardware/udev-rules/hdparm.nix
@@ -1,5 +1,4 @@
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   services.udev.extraRules = ''
     ACTION=="add|change", KERNEL=="sd[a-z]", ATTR{queue/rotational}=="1", \
       ATTRS{id/bus}=="ata", RUN+="${pkgs.hdparm}/bin/hdparm -B 254 -S 0 /dev/%k"
