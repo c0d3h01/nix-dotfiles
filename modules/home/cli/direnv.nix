151d45d0a9b8657f13bb40eb6a6e7c6c62d24161
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-15 15:14:14 +0000

    direnve;eza=enable bash and zsh ntegration

--- modules/home/cli/direnv.nix
+++ modules/home/cli/direnv.nix
@@ -3,6 +3,7 @@
     enable = true;
     silent = true;
     enableZshIntegration = true;
+    enableBashIntegration = true;
     nix-direnv.enable = true;
   };
 }
--- modules/home/cli/eza.nix
+++ modules/home/cli/eza.nix
@@ -1,7 +1,9 @@
 {
   programs.eza = {
     enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
     git = true;
-    icons = "auto";
+    icons = "always";
   };
 }
