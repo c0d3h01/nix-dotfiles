ce65497e4089a0d1117591e0451e42b227a4bf0a
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-15 22:21:29 -0500

    cleanup: home packages

--- modules/home/programs/default.nix
+++ modules/home/programs/default.nix
@@ -2,10 +2,8 @@
   imports = [
     ./git
     ./nvim
-    ./bun.nix
     ./diff-so-fancy.nix
     ./gemini-cli.nix
-    ./java.nix
     ./lazygit.nix
     ./package.nix
     ./spicetify.nix
--- modules/home/programs/package.nix
+++ modules/home/programs/package.nix
@@ -1,12 +1,7 @@
 {
-  lib,
   pkgs,
-  userConfig,
   ...
-}: let
-  inherit (lib) optionals;
-  isWorkstation = userConfig.workstation or false;
-in {
+}: {
   home.packages = with pkgs;
     [
       # Core CLI/dev tools
@@ -19,55 +14,16 @@ in {
       lazygit
       tree-sitter
       fd
-      mise
       sapling
       watchman
       just
       icdiff
-      jq
-      yq
       unzip
       zip
       file
-      which
       curl
       wget
       rsync
       htop
-      btop
-      dust
-      duf
-      hyperfine
-      tokei
-    ]
-    ++ optionals isWorkstation [
-      # Compilers/build tools
-      gnumake
-      cmake
-      ninja
-      pkg-config
-      clang
-      llvm
-      lld
-
-      # Language toolchains
-      python3
-      python3Packages.pip
-      nodejs_22
-      go
-      rustup
-
-      # Containers and infra
-      docker-client
-      docker-compose
-      kubectl
-      k9s
-
-      # Databases
-      postgresql
-      sqlite
-
-      # GUI dev helpers
-      insomnia
     ];
 }
