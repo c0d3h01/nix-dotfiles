88e9465e1fd7c55034f1c389fe41384cea875ac9
c0d3h01 <109863197+c0d3h01@users.noreply.github.com>
2026-02-16 03:22:23 +0000

    style: auto fmt

--- modules/home/programs/package.nix
+++ modules/home/programs/package.nix
@@ -1,29 +1,25 @@
-{
-  pkgs,
-  ...
-}: {
-  home.packages = with pkgs;
-    [
-      # Core CLI/dev tools
-      gitFull
-      gh
-      git-lfs
-      git-crypt
-      diffutils
-      delta
-      lazygit
-      tree-sitter
-      fd
-      sapling
-      watchman
-      just
-      icdiff
-      unzip
-      zip
-      file
-      curl
-      wget
-      rsync
-      htop
-    ];
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    # Core CLI/dev tools
+    gitFull
+    gh
+    git-lfs
+    git-crypt
+    diffutils
+    delta
+    lazygit
+    tree-sitter
+    fd
+    sapling
+    watchman
+    just
+    icdiff
+    unzip
+    zip
+    file
+    curl
+    wget
+    rsync
+    htop
+  ];
 }
