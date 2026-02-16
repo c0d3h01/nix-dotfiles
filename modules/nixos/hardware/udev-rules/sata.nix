07ab971170bb2cd65f46cfffb6b508a3a3a3e95f
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-16 05:36:30 +0000

    chore: sata rules to max performance default

--- modules/nixos/hardware/udev-rules/sata.nix
+++ modules/nixos/hardware/udev-rules/sata.nix
@@ -1,13 +1,8 @@
-{userConfig, ...}: let
-  alpmPolicy =
-    if (userConfig.laptop.enable or false)
-    then "med_power_with_dipm"
-    else "max_performance";
-in {
+{
   services.udev.extraRules = ''
     # SATA Active Link Power Management
     ACTION=="add", SUBSYSTEM=="scsi_host", KERNEL=="host*", \
       ATTR{link_power_management_policy}=="*", \
-      ATTR{link_power_management_policy}="${alpmPolicy}"
+      ATTR{link_power_management_policy}="max performance"
   '';
 }
