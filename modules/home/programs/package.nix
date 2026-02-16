714251c5aec3aa614b8cb6c740453d45406ed673
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-02-16 17:20:47 +0000

    chore: home user packages

--- modules/home/programs/package.nix
+++ modules/home/programs/package.nix
@@ -1,25 +1,10 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    # Core CLI/dev tools
-    gitFull
-    gh
-    git-lfs
-    git-crypt
-    diffutils
-    delta
-    lazygit
-    tree-sitter
-    fd
-    sapling
-    watchman
-    just
-    icdiff
-    unzip
-    zip
-    file
-    curl
-    wget
-    rsync
-    htop
+    freecad
+    openscad
+    kicad
+    cura-appimage
+    arduino-ide
+    graphviz
   ];
 }
