7d1c688bca2edaf252b3bb9bf3f494f9855700d3
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-03 17:15:51 +0530

    networking: simplify NetworkManager to stable defaults

--- modules/nixos/networking/network-manager.nix
+++ modules/nixos/networking/network-manager.nix
@@ -1,24 +1,3 @@
-{lib, ...}: {
-  services.resolved = {
-    enable = true;
-    dnssec = "allow-downgrade";
-    dnsovertls = "opportunistic";
-    fallbackDns = [
-      "1.1.1.1#cloudflare-dns.com"
-      "9.9.9.9#dns.quad9.net"
-    ];
-  };
-
-  networking.networkmanager = {
-    enable = true;
-    dns = "systemd-resolved";
-
-    wifi = {
-      powersave = lib.mkForce false;
-      macAddress = "random";
-      scanRandMacAddress = true;
-    };
-
-    ethernet.macAddress = "random";
-  };
+{
+  networking.networkmanager.enable = true;
 }
