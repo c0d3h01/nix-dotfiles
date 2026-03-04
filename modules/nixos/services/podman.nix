1e2cb60103b940399859ec9bbe56ee1a73a126fe
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-04 14:26:39 +0530

    services: remove comments from podman.nix

--- modules/nixos/services/podman.nix
+++ modules/nixos/services/podman.nix
@@ -1,4 +1,3 @@
-# Purpose: Podman rootless container runtime replacing Docker
 {
   pkgs,
   hostConfig,
@@ -7,13 +6,9 @@
   virtualisation.podman = {
     enable = true;
 
-    # Provide docker CLI compat (podman aliased as docker)
     dockerCompat = true;
-
-    # DNS resolution inside rootless containers
     defaultNetwork.settings.dns_enabled = true;
 
-    # Weekly auto-prune of unused images and volumes
     autoPrune = {
       enable = true;
       dates = "weekly";
@@ -21,15 +16,14 @@
     };
   };
 
-  # Rootless podman needs subuid/subgid ranges for user namespaces
   users.users.${hostConfig.username} = {
     autoSubUidGidRange = true;
   };
 
   environment.systemPackages = with pkgs; [
     podman-compose
-    buildah # OCI image builder
-    skopeo # container image inspection and transfer
-    lazydocker # TUI for container management (works with podman)
+    buildah
+    skopeo
+    lazydocker
   ];
 }
