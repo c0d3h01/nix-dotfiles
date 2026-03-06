f17fd8b83e5c5667677c545b945e35bb2e05d899
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-06 10:29:13 +0530

    refactor(home): use out-of-store symlink for live dotfiles updates

--- modules/home/dotfiles.nix
+++ modules/home/dotfiles.nix
@@ -1,53 +1,53 @@
 {
-  inputs,
+  config,
   lib,
   userConfig,
   ...
 }: let
-  inherit (inputs) dotfiles;
+  dotfiles = "${config.home.homeDirectory}/.dotfiles";
   isWorkstation = userConfig.workstation or false;
 
   configDirs =
     {
-      "nvim".source = "${dotfiles}/.config/nvim";
-      "shell".source = "${dotfiles}/.config/shell";
-      "cargo".source = "${dotfiles}/.config/cargo";
-      "gdb".source = "${dotfiles}/.config/gdb";
-      "lazygit".source = "${dotfiles}/.config/lazygit";
-      "bat".source = "${dotfiles}/.config/bat";
-      "fzf".source = "${dotfiles}/.config/fzf";
-      "lsd".source = "${dotfiles}/.config/lsd";
-      "ripgrep".source = "${dotfiles}/.config/ripgrep";
-      "nushell".source = "${dotfiles}/.config/nushell";
-      "yt-dlp".source = "${dotfiles}/.config/yt-dlp";
-      "nix".source = "${dotfiles}/.config/nix";
-      "nixpkgs".source = "${dotfiles}/.config/nixpkgs";
-      "pacman".source = "${dotfiles}/.config/pacman";
-      "flake8".source = "${dotfiles}/.config/flake8";
-      "isort.cfg".source = "${dotfiles}/.config/isort.cfg";
+      "nvim".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/nvim";
+      "shell".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/shell";
+      "cargo".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/cargo";
+      "gdb".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/gdb";
+      "lazygit".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/lazygit";
+      "bat".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/bat";
+      "fzf".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/fzf";
+      "lsd".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/lsd";
+      "ripgrep".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/ripgrep";
+      "nushell".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/nushell";
+      "yt-dlp".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/yt-dlp";
+      "nix".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/nix";
+      "nixpkgs".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/nixpkgs";
+      "pacman".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/pacman";
+      "flake8".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/flake8";
+      "isort.cfg".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/isort.cfg";
     }
     // lib.optionalAttrs isWorkstation {
-      "alacritty".source = "${dotfiles}/.config/alacritty";
-      "kitty".source = "${dotfiles}/.config/kitty";
-      "ghostty".source = "${dotfiles}/.config/ghostty";
-      "wezterm".source = "${dotfiles}/.config/wezterm";
-      "VSCodium/User".source = "${dotfiles}/.config/VSCodium/User";
-      "chrome-flags.conf".source = "${dotfiles}/.config/chrome-flags.conf";
-      "chromium-flags.conf".source = "${dotfiles}/.config/chromium-flags.conf";
+      "alacritty".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/alacritty";
+      "kitty".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/kitty";
+      "ghostty".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/ghostty";
+      "wezterm".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/wezterm";
+      "VSCodium/User".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/VSCodium/User";
+      "chrome-flags.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/chrome-flags.conf";
+      "chromium-flags.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.config/chromium-flags.conf";
     };
 
   rootFiles =
     {
-      ".gdbinit".source = "${dotfiles}/.gdbinit";
-      ".dircolors".source = "${dotfiles}/.dircolors";
-      ".zprofile".source = "${dotfiles}/.zprofile";
-      ".gitconfig".source = "${dotfiles}/.gitconfig";
-      ".gitignore".source = "${dotfiles}/.gitignore";
-      ".gitignore_global".source = "${dotfiles}/.gitignore_global";
-      ".gitattributes".source = "${dotfiles}/.gitattributes";
+      ".gdbinit".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.gdbinit";
+      ".dircolors".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.dircolors";
+      ".zprofile".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.zprofile";
+      ".gitconfig".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.gitconfig";
+      ".gitignore".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.gitignore";
+      ".gitignore_global".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.gitignore_global";
+      ".gitattributes".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.gitattributes";
     }
     // lib.optionalAttrs isWorkstation {
-      ".tmux.conf".source = "${dotfiles}/.tmux.conf";
+      ".tmux.conf".source = config.lib.file.mkOutOfStoreSymlink "${dotfiles}/.tmux.conf";
     };
 in {
   xdg.configFile = configDirs;
