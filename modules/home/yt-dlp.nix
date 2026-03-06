5a40a201852175af1cc3ffff46a66344ec852eeb
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-06 22:09:06 +0530

    feat(yt-dlp): add yt-dlp config and home module

---
+++ modules/home/yt-dlp.nix
@@ -0,0 +1,12 @@
+{
+  programs.yt-dlp = {
+    enable = true;
+    settings = {
+      embed-thumbnail = true;
+      embed-subs = true;
+      sub-langs = "all";
+      downloader = "aria2c";
+      downloader-args = "aria2c:'-c -x8 -s8 -k1M'";
+    };
+  };
+}
