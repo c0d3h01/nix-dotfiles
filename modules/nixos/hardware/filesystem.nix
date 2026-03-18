dd5172d82da9fecbb37c16088d6930d571e49a95
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-03-18 04:50:29 +0000

    refactored: droped xfs support; only btrfs maintaince

--- modules/nixos/hardware/filesystem.nix
+++ modules/nixos/hardware/filesystem.nix
@@ -4,58 +4,33 @@
   pkgs,
   ...
 }: let
-  inherit (lib) mkIf mkEnableOption mkMerge mkDefault;
-  cfg = config.dotfiles.nixos.hardware.filesystem;
+  inherit (lib) mkIf mkDefault;
   rootFs = config.fileSystems."/".fsType;
-  isXfs = rootFs == "xfs";
   isBtrfs = rootFs == "btrfs";
 in {
-  options.dotfiles.nixos.hardware.filesystem.enable = mkEnableOption "Filesystem scrubs and tools";
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = mkMerge [
-      (mkIf isXfs [pkgs.xfsprogs])
-      (mkIf isBtrfs [pkgs.btrfs-progs])
-    ];
+  environment.systemPackages = mkMerge [
+    (mkIf isBtrfs [pkgs.btrfs-progs])
+  ];
 
-    services.fstrim.enable = mkDefault true;
+  services.fstrim.enable = mkDefault true;
 
-    systemd.services.xfs-scrub = mkIf isXfs {
-      description = "XFS filesystem scrub for data integrity";
-      serviceConfig = {
-        Type = "oneshot";
-        Nice = 19;
-        IOSchedulingClass = "idle";
-        ExecStart = "${pkgs.xfsprogs}/bin/xfs_scrub -v /";
-      };
+  systemd.services.btrfs-scrub = mkIf isBtrfs {
+    description = "Btrfs filesystem scrub for data integrity";
+    serviceConfig = {
+      Type = "oneshot";
+      Nice = 19;
+      IOSchedulingClass = "idle";
+      ExecStart = "${pkgs.btrfs-progs}/bin/btrfs scrub start -B /";
     };
+  };
 
-    systemd.timers.xfs-scrub = mkIf isXfs {
-      enable = true;
-      timerConfig = {
-        OnCalendar = "weekly";
-        Persistent = true;
-      };
-      wantedBy = ["timers.target"];
+  systemd.timers.btrfs-scrub = mkIf isBtrfs {
+    enable = true;
+    timerConfig = {
+      OnCalendar = "weekly";
+      Persistent = true;
     };
-
-    systemd.services.btrfs-scrub = mkIf isBtrfs {
-      description = "Btrfs filesystem scrub for data integrity";
-      serviceConfig = {
-        Type = "oneshot";
-        Nice = 19;
-        IOSchedulingClass = "idle";
-        ExecStart = "${pkgs.btrfs-progs}/bin/btrfs scrub start -B /";
-      };
-    };
-
-    systemd.timers.btrfs-scrub = mkIf isBtrfs {
-      enable = true;
-      timerConfig = {
-        OnCalendar = "weekly";
-        Persistent = true;
-      };
-      wantedBy = ["timers.target"];
-    };
+    wantedBy = ["timers.target"];
   };
 }
