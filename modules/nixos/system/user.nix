2af5a623f9a9e1745466348cfc0987d79260fba4
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-04-03 19:59:53 +0530

    chore(nix-user): locale add US with IN; add more default user groups, rm old un necssasary groups.

--- modules/nixos/system/user.nix
+++ modules/nixos/system/user.nix
@@ -7,19 +7,22 @@
   networking.hostName = hostConfig.hostname;
   time.timeZone = "Asia/Kolkata";
 
-  # Locale (UTF-8)
-  i18n.defaultLocale = "en_IN";
+  # Locale
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocales = [ "en_IN/UTF-8" ];
   i18n.extraLocaleSettings = {
+    LC_CTYPE = "en_US.UTF-8";
+    LC_MESSAGES = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_COLLATE = "en_US.UTF-8";
     LC_ADDRESS = "en_IN";
     LC_IDENTIFICATION = "en_IN";
     LC_MEASUREMENT = "en_IN";
     LC_MONETARY = "en_IN";
     LC_NAME = "en_IN";
-    LC_NUMERIC = "en_IN";
     LC_PAPER = "en_IN";
     LC_TELEPHONE = "en_IN";
     LC_TIME = "en_IN";
-    LC_ALL = "en_IN";
   };
 
   # System State Version
@@ -29,9 +32,7 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
-  # Primary User Configuration
-  users.groups.${hostConfig.username} = {};
-
+  # User Configuration
   users.users.${hostConfig.username} = {
     isNormalUser = true;
     createHome = true;
@@ -40,10 +41,13 @@
     shell = pkgs.zsh;
 
     extraGroups = [
-      "wheel" # Sudo access
-      "audio" # Sound/PipeWire
-      "video" # GPU acceleration
-      "networkmanager" # Network control
+      "wheel"    # Admin privileges via sudo.
+      "adm"      # Read system logs (/var/log, journalctl).
+      "cdrom"    # Access optical drives (/dev/sr0).
+      "dip"      # Dial-up / PPP networking.
+      "plugdev"  # Access removable devices via udev.
+      "users"    # Default group for normal users.
+      "lpadmin"  # Manage printers via CUPS
     ];
   };
 }
