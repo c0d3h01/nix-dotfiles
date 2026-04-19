441da412c7b181eae11d85a2ca6fe4d487bed352
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-04-19 03:51:14 -0400

    filesystem: add home subvolume on btrfs

--- modules/nixos/filesystems/btrfs.nix
+++ modules/nixos/filesystems/btrfs.nix
@@ -9,6 +9,16 @@
     ];
   };
 
+  fileSystems."/home" = {
+    device = "/dev/disk/by-label/nix-root";
+    fsType = "btrfs";
+    options = [
+      "subvol=@home"
+      "noatime"
+      "compress=zstd:3"
+    ];
+  };
+
   fileSystems."/boot" = {
     device = "/dev/disk/by-label/nix-boot";
     fsType = "vfat";
