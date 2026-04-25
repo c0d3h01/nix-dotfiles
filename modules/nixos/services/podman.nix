9ff4168a848109e978bb90380b17989997d95ccb
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-04-25 06:11:16 +0000

    disable: podman container config

--- modules/nixos/services/podman.nix
+++ modules/nixos/services/podman.nix
@@ -3,12 +3,12 @@
     enable = true;
     dockerCompat = true;
 
-    containers.containersConf.settings = {
-      containers.dns_servers = [
-        "8.8.8.8"
-        "8.8.4.4"
-      ];
-    };
+    # containers.containersConf.settings = {
+    #   containers.dns_servers = [
+    #     "8.8.8.8"
+    #     "8.8.4.4"
+    #   ];
+    # };
   };
 
   environment.systemPackages = with pkgs; [
