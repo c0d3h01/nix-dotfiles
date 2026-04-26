c90281fd0e21036ea9cd7aeb7a55f361a49bd0c3
Harshal Sawant <harshalsawant.dev@gmail.com>
2026-04-26 11:36:53 +0530

    podman: droped commented configs

--- modules/nixos/services/podman.nix
+++ modules/nixos/services/podman.nix
@@ -2,13 +2,6 @@
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
-
-    # containers.containersConf.settings = {
-    #   containers.dns_servers = [
-    #     "8.8.8.8"
-    #     "8.8.4.4"
-    #   ];
-    # };
   };
 
   environment.systemPackages = with pkgs; [
